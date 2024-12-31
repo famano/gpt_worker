af98dacf70daba69f576b027ab7f460f4eecd968
Fumio Amano <t.yono.bookbug@gmail.com>
2024-12-31 22:56:37 +0900

    initial commit

---
+++ connector.py
@@ -0,0 +1,55 @@
+from abc import abstractmethod
+import json
+import openai
+from openai import OpenAI
+
+class Connector:
+    @abstractmethod
+    def CreateResponse(messages, tools):
+        pass
+
+class OpenAIConnector(Connector):
+
+    def CreateResponse(messages: list[str], tools: list[type], model: str="gpt-4o") -> list[str]:
+        llm = OpenAI()
+
+        print("messages:")
+        for message in messages:
+            print(message)
+        print("tools:")
+        print(tools)
+
+        response = llm.chat.completions.create(
+            model=model,
+            messages=messages,
+            tools=[openai.pydantic_function_tool(tool) for tool in tools]
+        )
+        # TODO: edge case
+
+        if response.choices[0].message.content != None:
+            messages.append({
+                "role": response.choices[0].message.role,
+                "content": response.choices[0].message.content
+            })
+
+        if response.choices[0].finish_reason == "tool_calls":
+            tool_calls = response.choices[0].message.tool_calls
+            messages.append({
+                "role": response.choices[0].message.role,
+                "tool_calls": [vars(tool_call) for tool_call in tool_calls]
+            })
+        
+            for tool_call in tool_calls:
+                arguments = json.loads(tool_call.function.arguments)
+                print("arguments")
+                print(arguments)
+                tool = next(filter(lambda tool: tool.__name__ == tool_call.function.name, tools), None)
+                content = tool.Run(arguments)
+                messages.append({
+                    "role": "tool",
+                    "content": json.dumps(content),
+                    "tool_call_id": tool_call.id
+                })
+            OpenAIConnector.CreateResponse(messages, tools, model)
+        
+        return messages
---
+++ tools.py
@@ -0,0 +1,62 @@
+from abc import abstractmethod
+import os
+import json
+from pydantic import BaseModel, Field
+
+class Tool(BaseModel):
+    @abstractmethod
+    def Run(args: dict) -> dict:
+        pass
+
+class FileReader(Tool):
+    "Read the contents of designated file. This function returns contents as text."
+    path: str = Field(..., description="relative path of target file to read.")
+    
+    def Run(args: dict) -> dict:
+        print("args:")
+        print(args)
+        with open(args["path"]) as f:
+            return {
+                "path": args["path"],
+                "content": f.read()
+            }
+
+class FileWriter(Tool):
+    "write given content to designated file. This function overwrites the file."
+    path: str = Field(..., description="relative path of target file to write.")
+    content: str = Field(..., description="content to write.")
+
+    def Run(args):
+        dir = os.path.dirname(args["path"])
+        if dir != "":
+            os.makedirs(dir, exist_ok=True)
+        with open(args["path"], mode="w") as f:
+            f.write(args["content"])
+        return {
+            "path": args["path"],
+            "success": True 
+        }
+
+class Task(BaseModel):
+    name: str
+    description: str
+    done_flg: bool
+
+class PlanMaker(Tool):
+    tasklist: list[Task]
+    path: str = Field(..., description="relative path of a file that you want to write a plan.")
+    def __init__(self, tasklist:list[Task], path: str =""):
+        super().__init__(tasklist=tasklist, path=path)
+        self.tasklist = tasklist
+        self.path = path
+
+    def Run(args: dict) -> dict:
+        dir = os.path.dirname(args["path"])
+        if dir != "":
+            os.makedirs(dir, exist_ok=True)
+        with open(args["path"], mode="w") as f:
+            f.write(json.dumps(args["tasklist"]))
+        return {
+            "path": args["path"],
+            "success": True,
+        }
